3355c2eaaae997b16ac5369bae02f97c1f3ed63f
Tobias Kolb <tkolb@recom.eu>
2025-01-17 14:00:08 +0100

    Initial version

---
+++ bcpexport.ps1
@@ -0,0 +1,79 @@
+# export data from db using definition in 'ContentDefinition.xml'
+# groups: enp icd10 ruleengine pkms
+# implicit: basics
+# future?: initial medi
+
+$global:dbServer    = "vidab-2"
+$global:dbUser      = "sa"
+$global:dbPassword  = "dev_sa"
+$global:dbName      = "MasterContent"
+$global:groupName   = "initial"
+
+function exportGroup {
+    param(
+        [string]$groupName,
+        [string]$dbName
+    )
+
+    Write-Host "`nDB export of group: $groupName`n"
+
+    $source = [xml](get-content "$pwd\ContentDefinition.xml")
+    $group = $source.SelectSingleNode("/ContentUpdate/Group[@name='$groupName']")
+    if (-not $null -eq $group)
+    {
+        $tables = $group.SelectNodes('Table')
+
+        foreach($table in $tables)
+        {
+            $tableName = $table.name
+            $clauseNode = $table.Item('Where')
+
+            Write-Host "Table: $tableName"
+
+            # export format xml
+            & bcp.exe "$dbName.dbo.$tableName" format nul -S $global:dbServer -U $global:dbUser -P $global:dbPassword -f ".\Export\$tableName.xml" -x -N
+
+            if ($null -eq $clauseNode)
+            {
+                #export completely
+                Write-Host "completely"
+
+                & bcp.exe "$dbName.dbo.$tableName" out ".\Export\$tableName.bcp" -S $global:dbServer -U $global:dbUser -P $global:dbPassword -N
+            }
+            else 
+            {
+                #export regarding clause
+                $clause = $clauseNode.InnerText
+                Write-Host "where $clause"
+
+                & bcp.exe "`"select * from $dbName.dbo.$tableName where $clause;`"" queryout  ".\Export\$tableName.bcp" -S $global:dbServer -U $global:dbUser -P $global:dbPassword -N
+            }
+
+            Write-Host "`n--------------------`n"
+        }
+
+        Write-Host "Clean up content definition`n"
+        $target = [xml](get-content "$pwd\ContentDefinition.xml")
+        $groups = $target.SelectNodes("/ContentUpdate/Group")
+        foreach ($g in $groups)
+        { 
+            if (-not ($groupName -eq $g.name))
+            {
+                Write-Host "remove $($g.name)"
+                $null = $g.ParentNode.RemoveChild($g)
+            }
+        }
+        $target.Save("$pwd\Export\ContentDefinition.xml")
+
+        Write-Host "`n--------------------`n"
+        Write-Host "Archive"
+
+        & 7z.exe a -t7z -sdel ".\Export\bulkdata.7z" ".\Export\*.xml" ".\Export\*.bcp"
+
+        Write-Host "`n--------------------`n"
+    }
+}
+
+Remove-Item ".\Export\*.*"
+
+exportGroup $global:groupName $global:dbName
---
+++ convert-dbupdate.ps1
@@ -0,0 +1,147 @@
+# convert 'dbupdate_*.xml'
+# groups: enp icd10 ruleengine symx pkms
+# implicit: basics (w/o group)
+# future?: initial medi (w/o group)
+
+if (-not ([System.Management.Automation.PSTypeName]'UtfStringWriter').Type)
+{
+    Add-Type -Language CSharp 
+@"
+public sealed class UtfStringWriter : System.IO.StringWriter
+{
+    public override System.Text.Encoding Encoding { get { return System.Text.Encoding.GetEncoding("UTF-8"); } }
+}
+"@;
+}
+
+$utfsw = New-Object UtfStringWriter
+$xmlWriter = New-Object System.XML.XMLTextWriter($utfsw)
+$xmlWriter.Formatting = 'Indented'
+$xmlWriter.Indentation = 1
+$xmlWriter.IndentChar = "`t"
+
+$xmlWriter.WriteStartDocument()
+$xmlWriter.WriteStartElement('ContentUpdate')
+$xmlWriter.WriteAttributeString('version', '1')
+
+
+function copyNode {
+    param(
+        [System.Xml.XmlNode]$table,
+        [System.Xml.XmlWriter]$xmlWriter,
+        [string]$fromname,
+        [string]$toname = $null,
+        [bool]$cdata = $false
+    )
+
+    if (-not $toname)
+    {
+        $toname = $fromname
+    }
+
+    $node = $table.Item($fromname);
+    if (-not $null -eq $node)
+    {
+        if ($cdata)
+        {
+            $xmlWriter.WriteStartElement($toname)
+            $xmlWriter.WriteCData($node.InnerText)
+            $xmlWriter.WriteEndElement()        
+        } 
+        else 
+        {
+            $xmlWriter.WriteElementString($toname, $node.InnerText)                
+        }        
+    }
+}
+
+function writeTable {
+    param(
+        [System.Xml.XmlNode]$table,
+        [System.Xml.XmlWriter]$xmlWriter
+    )
+    
+    $tableName = $table.'#text'.toUpper();
+
+    Write-Host "Table: $tableName"
+    
+    $xmlWriter.WriteStartElement('Table')
+    $xmlWriter.WriteAttributeString('name', $tableName)
+
+    copyNode $table $xmlWriter 'order' 'Order'
+    copyNode $table $xmlWriter 'limit' 'Where' $true
+
+    # to be clarified
+    copyNode $table $xmlWriter 'ignore' 
+    copyNode $table $xmlWriter 'alternateDelete' -cdata $true
+    copyNode $table $xmlWriter 'beforeSql' -cdata $true
+    
+    $xmlWriter.WriteEndElement() #Table
+}
+
+
+function writeGroup {
+    param(
+        [xml]$source,
+        [string]$targetGroup,
+        [System.Xml.XmlWriter]$xmlWriter
+    )
+
+    Write-Host "`nGroup: $targetGroup`n"
+
+    $xmlWriter.WriteComment("Content definitions for group '$targetGroup'")
+    $xmlWriter.WriteStartElement('Group')
+    $xmlWriter.WriteAttributeString('name', $targetGroup)
+
+    $nodes = $source.SelectNodes("/dbupdate/content/table")
+    foreach($table in $nodes)
+    {
+        $group = $table.Item('group');
+        $groupName = $group.InnerText;
+        
+        if ($targetGroup -eq 'initial' -and $null -eq $group)   
+        {
+            writeTable $table $xmlWriter
+        }
+    
+        if ($targetGroup -eq 'medi' -and $null -eq $group)   
+        {
+            writeTable $table $xmlWriter
+        }
+
+        if ($targetGroup -eq 'basics' -and $null -eq $group)   
+        {
+            writeTable $table $xmlWriter
+        }
+
+        if ($targetGroup -eq $groupName)
+        {
+            writeTable $table $xmlWriter
+        }
+    }
+    
+    $xmlWriter.WriteEndElement() #Group
+}
+
+$source = [xml](get-content "$pwd\dbupdate_initial.xml")
+writeGroup $source "initial" $xmlWriter
+
+$source = [xml](get-content "$pwd\dbupdate_master.xml")
+writeGroup $source "basics" $xmlWriter
+writeGroup $source "icd10" $xmlWriter
+writeGroup $source "pkms" $xmlWriter
+writeGroup $source "ruleengine" $xmlWriter
+
+$source = [xml](get-content "$pwd\dbupdate_medi.xml")
+writeGroup $source "medi" $xmlWriter
+
+
+$xmlWriter.WriteEndElement() #ContentUpdate
+
+
+$xmlWriter.WriteEndDocument()
+$xmlWriter.Flush()
+$xmlWriter.Close()
+
+$enc = [System.Text.Encoding]::GetEncoding("UTF-8");
+[System.IO.File]::WriteAllText("$pwd\ContentDefinition.xml", $utfsw.toString(), $enc)
